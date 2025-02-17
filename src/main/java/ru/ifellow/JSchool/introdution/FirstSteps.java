6b7ad1f4c8a4e0109df3c3a4178648acea922437
gemansipator <gemansipator@gmail.com>
2025-02-17 13:08:42 +0300

    16 задач решены

--- src/main/java/ru/ifellow/JSchool/introdution/FirstSteps.java
+++ src/main/java/ru/ifellow/JSchool/introdution/FirstSteps.java
@@ -3,66 +3,127 @@ package ru.ifellow.JSchool.introdution;
 public class FirstSteps {
 
     public int sum (int x, int y){
-
+        return x + y;
     }
 
     public int mul (int x, int y){
+        return x * y;
 
     }
 
     public int div (int x, int y){
-
+        if(y == 0){
+            System.out.println("На ноль делить нельзя");
+        }
+        return x / y;
     }
 
     public int mod (int x, int y){
-
+        if(y == 0){
+            System.out.println("На ноль делить нельзя");
+        }
+        return x % y;
     }
 
     public boolean isEqual (int x, int y){
-
+        return x == y;
     }
 
     public boolean isGreater (int x, int y){
-
+        return x > y;
     }
 
     public boolean isInsideRect(int xLeft, int yTop, int xRight, int yBottom, int x, int y){
-
+        return (x >= xLeft && y >= yTop) && (x <= xRight && y <= yBottom);
     }
 
     public int sum(int[] array){
+        int sum = 0;
+        for(int i : array){
+            sum += i;
+        }
+        return sum;
 
     }
 
     public int mul(int[] array){
-
+        int mul = 1;
+        for(int i : array){
+            mul *= i;
+        }
+        return mul;
     }
 
     public int min(int[] array){
-
+        int min = Integer.MAX_VALUE;
+        for(int i : array){
+            if(i < min){
+                min = i;
+            }
+        }
+        return min;
     }
 
     public int max(int[] array){
+        int max = Integer.MIN_VALUE;
+        for(int i : array){
+            if(i > max){
+                max = i;
+            }
+        }
+        return max;
 
     }
 
     public double average(int[] array){
-
+        if (array.length == 0)
+            return 0;  //
+        int sum = 0;
+        for(int i : array){
+            sum += i;
+        }
+        return (double)sum / (double)array.length;
     }
 
-    public boolean isSortedDescendant(int[] array){
+    public boolean isSortedDescendant(int[] array) {
+        if (array.length == 0) {
+            return true; // всё по порядку
+        }
 
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] < array[i + 1]) {
+                return false; // Если нашли возрастание — не убывающий порядок
+            }
+        }
+
+        return true;
     }
 
     public void cube(int[]array){
 
+        for(int i = 0; i < array.length; i++) {
+            array[i] = array[i] * array[i] * array[i];
+        }
     }
 
     public boolean find(int[]array, int value){
+        for(int i = 0; i < array.length; i++){
+            if(array[i] == value) {
+                return true;
+            }
+        }
+        return false;
 
     }
 
+    /*public void reverse(int[]array) Переворачивает одномерный массив array, то есть
+    меняет местами 0-й и последний, 1-й и предпоследний и т.д. элементы.*/
     public void reverse(int[]array){
+        for(int i = 0; i < array.length/2; i++){
+            int temp = array[i];
+            array[i] = array[array.length - i - 1];
+            array[array.length - i - 1] = temp;
+        }
 
     }
 
